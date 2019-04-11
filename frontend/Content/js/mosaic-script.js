d8d269c3a20099439235ea34158c4d239527e371
Rodrigo Soares <rodrigo.soares@avalara.com>
2019-04-11 11:35:12 -0300

    Tech debt

--- frontend/Content/js/mosaic-script.js
+++ frontend/Content/js/mosaic-script.js
@@ -466,7 +466,7 @@ function ShowModalSucess(dataHtml) {
 
 jQuery.extend({
     compare: function (arrayA, arrayB) {
-        if (arrayA.length != arrayB.length) { return false; }
+        if (arrayA.length !== arrayB.length) { return false; }
         // sort modifies original array
         // (which are passed by reference to our method!)
         // so clone the arrays before sorting
@@ -510,18 +510,18 @@ function GetCurrentCustomer() {
 function CNPJCheck(c) {
     var b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
 
-    if ((c = c.replace(/[^\d]/g, "")).length != 14)
+    if ((c = c.replace(/[^\d]/g, "")).length !== 14)
         return false;
 
     if (/0{14}/.test(c))
         return false;
 
     for (var i = 0, n = 0; i < 12; n += c[i] * b[++i]);
-    if (c[12] != (((n %= 11) < 2) ? 0 : 11 - n))
+    if (c[12] !== ((n %= 11) < 2 ? 0 : 11 - n))
         return false;
 
     for (var i = 0, n = 0; i <= 12; n += c[i] * b[i++]);
-    if (c[13] != (((n %= 11) < 2) ? 0 : 11 - n))
+    if (c[13] !== ((n %= 11) < 2 ? 0 : 11 - n))
         return false;
 
     return true;
